84fc3170fb47068d6f8e22601f1ffca2d3f1acc8
Nitai Charan <nitaicharan@gmail.com>
2019-12-24 23:26:33 -0300

    12. Testando a primeira classe de domínio - Categoria
    @Data
    @AllArgsConstructor
    @NoArgsConstructor
    https://medium.com/dev-cave/reduzindo-c%C3%B3digo-com-projeto-lombok-utilizando-intellij-a7711c2212bb

---
+++ src/main/java/com/nelioalves/cursomc/domain/Categoria.java
@@ -0,0 +1,12 @@
+package com.nelioalves.cursomc.domain;
+
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Categoria {
+
+  public Integer id;
+  public String nome;
+}
