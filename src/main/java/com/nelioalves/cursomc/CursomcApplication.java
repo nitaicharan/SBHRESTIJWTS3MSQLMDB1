01e8cb91c516b5ce036f1abf99b0ccf295d40848
Nitai Charan <nitaicharan@gmail.com>
2020-07-05 16:04:30 -0300

    19. Produto e associacao muitos para muitos

--- src/main/java/com/nelioalves/cursomc/CursomcApplication.java
+++ src/main/java/com/nelioalves/cursomc/CursomcApplication.java
@@ -1,9 +1,12 @@
 package com.nelioalves.cursomc;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import com.nelioalves.cursomc.domain.Categoria;
+import com.nelioalves.cursomc.domain.Produto;
 import com.nelioalves.cursomc.repositories.CategoriaRepository;
+import com.nelioalves.cursomc.repositories.ProdutoRepository;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -16,6 +19,7 @@ import lombok.AllArgsConstructor;
 public class CursomcApplication implements CommandLineRunner {
 
 	private CategoriaRepository categoriaRepository;
+	private ProdutoRepository produtoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -23,10 +27,21 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática", new ArrayList<Produto>());
+		Categoria cat2 = new Categoria(null, "Escritório", new ArrayList<Produto>());
 
-		Categoria cat1 = new Categoria(null, "Informática");
-		Categoria cat2 = new Categoria(null, "Escritório");
+		Produto p1 = new Produto(null, "Computador", 2000.00, new ArrayList<Categoria>());
+		Produto p2 = new Produto(null, "Impressora", 800.00, new ArrayList<Categoria>());
+		Produto p3 = new Produto(null, "Mouse", 80.00, new ArrayList<Categoria>());
+
+		cat1.setProdutos(Arrays.asList(p1, p2, p3));
+		cat2.setProdutos(Arrays.asList(p2));;
+		
+		p1.setCategorias(Arrays.asList(cat1));;
+		p2.setCategorias(Arrays.asList(cat1, cat2));;
+		p3.setCategorias(Arrays.asList(cat1));;
 
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 	}
 }
--- src/main/java/com/nelioalves/cursomc/domain/Produto.java
+++ src/main/java/com/nelioalves/cursomc/domain/Produto.java
@@ -8,6 +8,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
 import lombok.AllArgsConstructor;
@@ -22,14 +24,16 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode
-public class Categoria implements Serializable {
-  private static final long serialVersionUID = 1L;
+public class Produto implements Serializable {
+	private static final long serialVersionUID = 1L;
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  public Integer id;
-  public String nome;
-  
-  @ManyToMany(mappedBy = "categorias")
-  private List<Produto> produtos = new ArrayList<>();
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String nome;
+	private Double preco;
+
+	@ManyToMany
+	@JoinTable(name = "PRODUTO_CATEGORIA", joinColumns = @JoinColumn(name = "produto_id"), inverseJoinColumns = @JoinColumn(name = "categoria_id"))
+	private List<Categoria> categorias = new ArrayList<>();
 }
